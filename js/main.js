e64ae7adc4e4725ba04e558e8117d33e1ada992e
choi_hyeona <hac0309@naver.com>
2024-03-06 16:48:15 +0900

    240306 헤더 , 로고, 서치, 머트리얼 아이콘, js 강제 포커스 기타 등등

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// 검색 영역(.search) 클릭 시 input에 강제 포커스 및 제어
+// 검색 영역 div와 input 찾기
+const searchEl = document.querySelector('.search');
+// const searchInputEl = document.querySelector('.search input') // 문서 전체에서 찾지 말고 아래와 같이 최적화
+const searchInputEl = searchEl.querySelector('input');
+
+// 검색 영역을 클릭하면 input 요소를 포커스하도록 실행
+
+searchEl.addEventListener('click', function () { //이벤트 핸들러
+  searchInputEl.focus(); // 요소에 포커스 강제 적용
+});
